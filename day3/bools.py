89006701ce053e369361911cdcd81d24a30f6b19
Quaker69 <melwinwilson39@gmail.com>
2023-06-22 00:53:48 +0530

    say 3 almost done

--- day3/bools.py
+++ day3/bools.py
@@ -1,4 +1,4 @@
-"""""
+
 print( 'gabe' is 'gabe')  
 # shitty use of  "is"        
 print('gabe' is not 'apple') 
@@ -28,9 +28,7 @@ c=int(input("Enter side c :"))
 print("The perimeter of the triangle is ",a+b+c)
 
 #finding the val of x 
-"""
 
-""""
 
 
 x=1
@@ -43,8 +41,6 @@ while True:
         x =+ 1
 print(x)
 
-"""
-
 def eqn(x):
     y = x ** 2 + 6*x + 9
     return y
@@ -55,8 +51,45 @@ while True:
         break
     else: x += 1
 print(x)
-    
-       
+   
+#falsi comparision
 
+print('python' is 'dragon')
+
+
+print('on' in ('python ' and 'dragon'))
+
+#find jargon
+print("its exist",'jargon' in 'I hope this course is not full of jargon')
+
+# len of python in float in string
+print(float(len("python")))
     
-  
+#no on in dragon
+print('on' not in ('python ' and 'dragon'))
+
+
+# fucn for even check
+def even(n):
+    if(n%2==0):
+        return True
+    
+#floor divison
+if(float(7//3==2.7)):
+    print(True)
+
+#int check
+if(int(9.8)==10):
+    print(True)
+
+#Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+
+hours=int(input("Enter hours :"))
+rate=int(input("Enter rate per hour :"))
+print("Your weekly earning is ",int(hours* rate))
+
+#Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+
+
+
+
